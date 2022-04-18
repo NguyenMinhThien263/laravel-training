b749b73d5ee735e358efd9dd0af20abe77eae7e3
NguyenMinhThien263 <nguyenminhthien263@gmail.com>
2022-04-18 13:01:56 +0700

    push soure

---
+++ resources/views/tdc.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('web2/styleTDC.css')}}">
+    <title>Hello World</title>
+</head>
+<body>
+    <nav>
+        <div class="catenav navbar-collapse">
+
+            <ul>
+                <li>Chuyên đề</li>
+                <li>Tin học cơ sở</li>
+                <li>Công nghệ phần mềm</li>
+                <li>Đồ hoạ</li>
+                <li>Mạng máy tính</li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
